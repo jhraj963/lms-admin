18c2221786594e3bbb7e759eb60df3a997ecaa61
jhraj963 <jhraj963@gmail.com>
2025-03-02 15:59:39 +0600

    server api

--- src/http-common.js
+++ src/http-common.js
@@ -1,22 +1,8 @@
 
-import axios from "axios";
-
-const instance = axios.create({
-  baseURL: "http://127.0.0.1:8000/api", // Replace with your actual API base URL
-  headers: {
-    "Content-Type": "application/json",
-    Authorization: `Bearer ${sessionStorage.getItem("token")}`, // Add token if available
-  },
-});
-
-export default instance;
-
-
-
 // import axios from "axios";
 
 // const instance = axios.create({
-//   baseURL: "https://weeblecode.com/eshopper/public/api", // Replace with your actual API base URL
+//   baseURL: "http://127.0.0.1:8000/api", // Replace with your actual API base URL
 //   headers: {
 //     "Content-Type": "application/json",
 //     Authorization: `Bearer ${sessionStorage.getItem("token")}`, // Add token if available
@@ -24,4 +10,18 @@ export default instance;
 // });
 
 // export default instance;
+
+
+
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: "https://weeblecode.com/eshopper/public/api", // Replace with your actual API base URL
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${sessionStorage.getItem("token")}`, // Add token if available
+  },
+});
+
+export default instance;
 
