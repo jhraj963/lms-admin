0d586f326d761276e68eee3c8ea7f60a24aa97a5
jhraj963 <jhraj963@gmail.com>
2025-03-02 16:08:55 +0600

    lms

--- src/http-common.js
+++ src/http-common.js
@@ -16,7 +16,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://weeblecode.com/eshopper/public/api", // Replace with your actual API base URL
+  baseURL: "https://weeblecode.com/lms/public/api", // Replace with your actual API base URL
   headers: {
     "Content-Type": "application/json",
     Authorization: `Bearer ${sessionStorage.getItem("token")}`, // Add token if available
